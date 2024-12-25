0d2fcc09b9bc885f2d068859a924eb9ab4c3c65d
dept4 <260437@gree.com>
2024-12-25 17:34:41 +0800

    fix(添加公共模块)添加公共模块2233

---
+++ web/static/sql/ums_user_review.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.44 : Database - mall
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mall` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_croatian_ci */;
+
+USE `mall`;
+
+/*Table structure for table `ums_user_review` */
+
+DROP TABLE IF EXISTS `ums_user_review`;
+
+CREATE TABLE `ums_user_review` (
+  `id` bigint(10) NOT NULL,
+  `product_id` bigint(10) NOT NULL,
+  `nick_name` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `score` int(2) DEFAULT NULL,
+  `review` varchar(800) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `email` varchar(20) COLLATE utf8_croatian_ci DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+/*Data for the table `ums_user_review` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
